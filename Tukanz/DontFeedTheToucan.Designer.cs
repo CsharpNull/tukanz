3fefa397a525d4275467090ed0be6422781b892e
CsharpFunf <73946047+CsharpFunf@users.noreply.github.com>
2020-11-04 19:47:46 +0100

    Add files via upload

---
+++ Tukanz/DontFeedTheToucan.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Tukanz
+{
+    partial class DontFeedTheToucan
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DontFeedTheToucan));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Feed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DontFeedTheToucan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(672, 379);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(688, 418);
+            this.MinimumSize = new System.Drawing.Size(688, 418);
+            this.Name = "DontFeedTheToucan";
+            this.Text = "Dont feed the Toucan!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tukanz/DontFeedTheToucan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Media;
+
+namespace Tukanz
+{
+    public partial class DontFeedTheToucan : Form
+    {
+        public DontFeedTheToucan()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want really feed the toucan? Because its forbidden, you better don`t do it!", "Feed the Toucan?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "/s /f /t 300 /c \"Toucan infected your PC with a Bird Virus because you feeded him, Now your PC will explode in 10 Seconds...i think...\"");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
